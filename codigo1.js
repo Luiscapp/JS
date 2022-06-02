b8c55c9e93c60711cf9cae10530f0920ba64e3c2
Luis Pinto <Luiscapp17@gmail.com>
2022-06-02 18:24:51 -0400

    subiendo tarea 2

---
+++ codigo1.js
@@ -0,0 +1,10 @@
+const nombre = "Luis"
+const edad = 21
+const desarrollador = false
+const nacimiento = new Date("march 25 2001")
+const libro = {
+  titulo: "Once Minutos",
+  autor: "Paulo Coelho",
+  fecha: 2003,
+  url: "https://www.amazon.com/-/es/Paulo-Coelho/dp/0060591838"
+}
