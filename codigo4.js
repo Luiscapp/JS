08d864f3cf8bb744b3293e0738d33d295b3461bf
Luis Pinto <Luiscapp17@gmail.com>
2022-06-07 16:02:15 -0400

    Subiendo tarea 4

---
+++ codigo4.js
@@ -0,0 +1,9 @@
+let nombre = "Luis"
+let apellido = "Pinto"
+let estudiante = console.log(nombre.concat(" ", apellido))
+let estudianteMayus = console.log(estudiante.toUpperCase())
+let estudianteMinus = console.log(estudiante.toLowerCase())
+let caracteres = console.log(estudiante.lenght())
+console.log(nmbre.chartAt(0))
+console.log(apellido.charAt(0))
+console.log(estudiante.trim().length)
