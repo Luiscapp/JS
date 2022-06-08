304131ffa852c7048ff10446e45c3cdf51834985
Luis Pinto <Luiscapp17@gmail.com>
2022-06-08 12:39:01 -0400

    actualizando tarea 5

--- codigo5.js
+++ codigo5.js
@@ -1,5 +1,6 @@
 const alturaCm = 168
 const alturaM = 1.68
 const peso = 58.5
-const alturaRedondeada = console.log(Math.round(1.68))
-const pesoRedondeado = console.log(Math.floor(58.5))
+const alturaRedondeada = Math.ceil(1.68)
+const pesoRedondeado = Math.floor(58.5)
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
