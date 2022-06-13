f208002f006ee1e9466dc395364c655f9f71985f
Luis Pinto <Luiscapp17@gmail.com>
2022-06-13 18:49:23 -0400

    Subiendo tarea 9

---
+++ fechas.js
@@ -0,0 +1,7 @@
+const fecha = new Date()
+const nacimiento = new Date(2001, 2, 17)
+
+console.log(fecha > nacimiento)
+console.log(nacimiento.getDate())
+console.log(nacimiento.getMonth() + 1)
+console.log(nacimiento.getFullYear())
