a60a8a42b045caad381af534090323513297c4c3
Luis Pinto <Luiscapp17@gmail.com>
2022-06-13 18:10:25 -0400

    Subiendo tarea 7

---
+++ codigo7.js
@@ -0,0 +1,8 @@
+const familia = [
+    { nombre: "Luis" },
+    { nombre: "Yaid" },
+    { nombre: "Juan" },
+]
+const miSet = new Set ( familia )
+miSet.add ( { nombre: "JavaScript" } )
+console.log ( miSet )
