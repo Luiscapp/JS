85ef2dcbb183d18b4612d80f561bb08b8bd68f32
Luis Pinto <Luiscapp17@gmail.com>
2022-06-14 13:44:18 -0400

    Subiendo tarea 10

---
+++ codigo10.js
@@ -0,0 +1,14 @@
+saludar()
+
+function saludar() {
+    return true,
+    console.log(saludar)
+}
+
+const promesa = new Promise((resolve, reject) => {
+    if (true) {
+        resolve()
+    } else {
+        reject()
+    }
+})
